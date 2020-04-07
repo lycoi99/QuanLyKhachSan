1637361d87e92078bafd9818f97900e51ae4f73f
lycoi99 <62951418+lycoi99@users.noreply.github.com>
2020-04-07 23:30:31 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿USE [QuanLyKhachSan]
+GO
+
+Insert into Phong(MaPhong, TenPhong, DonGia) values ('01','Phòng 1','1000000')
+Insert into Phong(MaPhong, TenPhong, DonGia) values ('02','Phòng 2', '500000')
+
+
